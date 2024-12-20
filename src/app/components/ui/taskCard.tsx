ed4008ca0ef808839d35ae603a192c1badf6a260
Churchil Owino <chachowino75@gmail.com>
2024-12-20 14:13:35 +0300

    Break component into a sub-component

--- src/app/components/ui/taskCard.tsx
+++ src/app/components/ui/taskCard.tsx
@@ -1,29 +1,16 @@
 "use client";
+import { Task } from "@/models/task";
+import TaskCardItem from "./taskCardItem";
 
-import { useRouter } from "next/navigation";
-import { FaRegCircle, FaRegCheckCircle } from "react-icons/fa";
-import { HiOutlineTrash } from "react-icons/hi";
-
-// To receive props (task details) and render them to the screen
-const TaskCard = () => {
-  const router = useRouter();
+interface TaskCardProps {
+  tasks: Task[];
+}
+const TaskCard: React.FC<TaskCardProps> = ({ tasks }) => {
   return (
-    <div
-      className="w-[90%] lg:w-[736px] border-[1px] border-badge-gray rounded-lg bg-task-card-bg p-4 flex items-center justify-between cursor-pointer gap-x-2"
-      onClick={() => router.push("/tasks/taskId")}
-    >
-      <FaRegCircle
-        size={18}
-        className="text-light-blue cursor-pointer hover:text-dark-blue min-w-6"
-      />
-      <p className="font-normal text-sm text-button-text">
-        Integer urna interdum massa libero auctor neque turpis turpis semper.
-        Duis vel sed fames integer.
-      </p>
-      <HiOutlineTrash
-        size={22}
-        className="text-text-gray hover:bg-badge-gray cursor-pointer hover:border rounded-lg hover:border-transparent hover:text-red-500 min-w-6"
-      />
+    <div className="space-y-4">
+      {tasks.map((task) => (
+        <TaskCardItem key={task.id} task={task} />
+      ))}
     </div>
   );
 };
