3d093ba87710d291d3f6534899850eae14a28acd
Churchil Owino <chachowino75@gmail.com>
2024-12-20 14:11:36 +0300

    Set up pagination

--- src/app/(routes)/(home)/components/homeClient.tsx
+++ src/app/(routes)/(home)/components/homeClient.tsx
@@ -3,11 +3,25 @@ import Button from "@/app/components/ui/button";
 import CountBadge from "@/app/components/ui/countBadge";
 import NoTasksUI from "@/app/components/ui/noTasksUI";
 import TaskCard from "@/app/components/ui/taskCard";
+import { TaskData } from "@/models/task";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { GoPlusCircle } from "react-icons/go";
 
-const HomeClient = () => {
+interface HomeClientProps {
+  tasksData: TaskData;
+  page: number;
+  pageSize: number;
+}
+
+const HomeClient: React.FC<HomeClientProps> = ({
+  tasksData,
+  page,
+  pageSize,
+}) => {
   const router = useRouter();
+  const pageNumber = typeof page === "string" ? Number(page) : 1;
+  const pageSizeNumber = typeof pageSize === "string" ? Number(pageSize) : 5;
   return (
     <main className="w-full flex flex-col items-center justify-center">
       <Button className="-mt-6" onClick={() => router.push("/tasks/new")}>
@@ -17,20 +31,50 @@ const HomeClient = () => {
         <div className="flex items-center justify-between w-full">
           <div className="flex items-center gap-x-2">
             <span className="text-light-blue font-bold text-sm">Tasks</span>
-            <CountBadge count={0} />
+            <CountBadge count={tasksData.total} />
           </div>
           <div className="flex items-center gap-x-2">
             <span className="text-lavender-blue font-bold text-sm">
               Completed
             </span>
-            <CountBadge count={0} />
+            <CountBadge count={tasksData.completed} />
           </div>
         </div>
       </div>
-      <NoTasksUI />
+      {/* If no tasks found, render NoTaskUI component */}
+      {tasksData.total === 0 && <NoTasksUI />}
       <div className="mt-6 flex justify-center">
-        <TaskCard />
+        <TaskCard tasks={tasksData.tasks} />
       </div>
+      {tasksData.total > 5 && (
+        <div className="mt-8 space-x-6">
+          <Link
+            href={{
+              query: {
+                page: pageNumber > 1 ? pageNumber - 1 : 1,
+              },
+            }}
+            className={`py-2 px-3 bg-transparent border border-white rounded-lg text-white hover:shadow-xl hover:border-text-gray hover:text-text-gray ${
+              pageNumber <= 1 && "pointer-events-none opacity-50"
+            }`}
+          >
+            Previous
+          </Link>
+          <Link
+            href={{
+              query: {
+                page: pageNumber + 1,
+              },
+            }}
+            className={`py-2 px-3 bg-transparent border border-white rounded-lg text-white hover:shadow-xl hover:border-text-gray hover:text-text-gray ${
+              tasksData.tasks.length / pageSizeNumber < 1 &&
+              "pointer-events-none opacity-50"
+            }`}
+          >
+            Next
+          </Link>
+        </div>
+      )}
     </main>
   );
 };
