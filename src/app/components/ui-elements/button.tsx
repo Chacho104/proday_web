86f2f28692878869c411bed1e677235787dc2df5
Churchil Owino <chachowino75@gmail.com>
2025-01-10 17:37:40 +0300

    Create a reusable button component

---
+++ src/app/components/ui-elements/button.tsx
@@ -0,0 +1,29 @@
+import { cn } from "@/app/lib/util";
+import { forwardRef } from "react";
+
+// A reusable button component which allows dynamic content to be passed in as label
+// In screens larger than 1020px, button has a fixed width of 736px. In smaller screens,
+// the button occupies 90% of available width
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, disabled, type, ...props }, ref) => {
+    return (
+      <button
+        ref={ref}
+        {...props}
+        className={cn(
+          `bg-dark-blue hover:bg-light-blue p-4 rounded-lg text-button-text text-sm font-bold flex items-center justify-center gap-x-2 w-[90%] lg:w-[736px]`,
+          className
+        )}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+Button.displayName = "Button";
+
+export default Button;
