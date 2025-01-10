53e119ef89b1560deb70da3e5953dc4d2b550e6b
Churchil Owino <chachowino75@gmail.com>
2025-01-10 17:28:05 +0300

    Refactor code Delete unneeded files

--- src/lib/util.ts
+++
@@ -1,7 +0,0 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-// Function to merge tailwind css classnames
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
--- src/providers/toastProvider.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import React from "react";
-
-import { Toaster } from "react-hot-toast";
-
-export const ToasterProvider = () => {
-  return <Toaster />;
-};
