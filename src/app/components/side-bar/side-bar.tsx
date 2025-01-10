cc6352446a7ed6e934c91b385291baece233bb22
Churchil Owino <chachowino75@gmail.com>
2025-01-10 17:31:53 +0300

    Create side bar component

---
+++ src/app/components/side-bar/side-bar.tsx
@@ -0,0 +1,12 @@
+const SideBar = () => {
+  return (
+    <div className="bg-cards-background h-full rounded-md max-w-[25%] shadow-md">
+      <div>Calendar</div>
+      <div>Suggested strategy to achieve daily goal</div>
+      <div>Settings</div>
+      <div>Log out button</div>
+    </div>
+  );
+};
+
+export default SideBar;
