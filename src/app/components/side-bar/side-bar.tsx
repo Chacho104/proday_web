2159cf07e3b10b0e45efa974a708491fc8b4f628
Churchil Owino <chachowino75@gmail.com>
2025-01-11 21:58:55 +0300

    Remove elements of sidebar

--- src/app/components/side-bar/side-bar.tsx
+++ src/app/components/side-bar/side-bar.tsx
@@ -1,10 +1,9 @@
+// Simple component to show today's date, a welcome message, and a strategy if available
 const SideBar = () => {
   return (
-    <div className="bg-cards-background h-full rounded-md max-w-[25%] shadow-md">
+    <div className="bg-cards-background h-full rounded-md max-w-[25%] shadow-lg p-4 hidden lg:block">
       <div>Calendar</div>
       <div>Suggested strategy to achieve daily goal</div>
-      <div>Settings</div>
-      <div>Log out button</div>
     </div>
   );
 };
