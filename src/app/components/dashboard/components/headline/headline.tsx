356c0dc11b49e2af49d7f8f96a386221a0c8d344
Churchil Owino <chachowino75@gmail.com>
2025-01-11 21:56:50 +0300

    Create the headline component

---
+++ src/app/components/dashboard/components/headline/headline.tsx
@@ -0,0 +1,11 @@
+// Simple component to render Proday logo and headline statement
+
+const Headline = () => {
+  return (
+    <div>
+      <h1 className="text-white text-4xl font-semibold">Proday</h1>
+    </div>
+  );
+};
+
+export default Headline;
