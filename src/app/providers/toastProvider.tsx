8f537921cf9e9b1add4481e5a424b5fd65afa59c
Churchil Owino <chachowino75@gmail.com>
2025-01-11 22:00:16 +0300

    Add comments to make code clear

--- src/app/providers/toastProvider.tsx
+++ src/app/providers/toastProvider.tsx
@@ -4,6 +4,8 @@ import React from "react";
 
 import { Toaster } from "react-hot-toast";
 
+// Provider function that returns the toaster from react-hot-toast
+// Mostly used for notifications that follow user interaction
 export const ToasterProvider = () => {
   return <Toaster />;
 };
