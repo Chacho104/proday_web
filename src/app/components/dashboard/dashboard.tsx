6fd8db860eebc4b504f0f6f67688ed4bd4274d4a
Churchil Owino <chachowino75@gmail.com>
2025-01-11 21:56:00 +0300

    Build up dashboard component

--- src/app/components/dashboard/dashboard.tsx
+++ src/app/components/dashboard/dashboard.tsx
@@ -1,13 +1,14 @@
 // Dashboard component - renders all the information/components needed to create
 // and manage user tasks
 
-import Logo from "./components/logo";
+import Headline from "./components/headline/headline";
+import UserMenu from "./components/user-menu/user-menu";
 
 const Dashboard = () => {
   return (
-    <div className="flex items-center justify-between">
-      <Logo />
-      <div>User menu</div>
+    <div className="flex items-center justify-between w-full py-4">
+      <Headline />
+      <UserMenu />
     </div>
   );
 };
