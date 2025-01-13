93df624990c8f92b32eaa052ba9848f4e86b775d
Churchil Owino <chachowino75@gmail.com>
2025-01-13 18:40:44 +0300

    Change location of logo component

---
+++ src/app/[userId]/components/nav-bar/components/logo.tsx
@@ -0,0 +1,11 @@
+// Simple component to render Proday logo
+
+const Logo = () => {
+  return (
+    <div>
+      <h1 className="text-white text-4xl font-semibold">Proday</h1>
+    </div>
+  );
+};
+
+export default Logo;
