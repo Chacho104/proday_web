3f9e99c4668a9a929d3b5104af9c4925db3020d3
Churchil Owino <chachowino75@gmail.com>
2025-01-13 18:47:13 +0300

    Change text color of count

--- src/app/components/ui-elements/general/badged-icon.tsx
+++ src/app/components/ui-elements/general/badged-icon.tsx
@@ -13,7 +13,7 @@ const BadgedIcon: React.FC<BadgedIconProps> = ({ icon: Icon, count }) => {
     <div className="flex items-center cursor-pointer hover:scale-110">
       <Icon size={34} className="text-white" />
       <div className="bg-warm-yellow h-5 w-5 rounded-full flex items-center justify-center -ml-3 -mt-5 text-white">
-        <span className="text-[10px]">{count}</span>
+        <span className="text-[10px] text-black">{count}</span>
       </div>
     </div>
   );
