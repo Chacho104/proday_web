929c09435eed93fcd6026302c04794d127a33b44
Churchil Owino <chachowino75@gmail.com>
2025-01-13 18:41:43 +0300

    Change location of component

---
+++ src/app/[userId]/components/side-bar/side-bar.tsx
@@ -0,0 +1,15 @@
+// Simple component to show today's date, a welcome message, and a strategy if available
+
+import Card from "@/app/components/ui-elements/general/card";
+
+const Sidebar = () => {
+  return (
+    <Card className="h-full w-[30%] shadow-md p-4 hidden lg:block">
+      <div>Calendar</div>
+      <div>Guiding Quote Of The Day</div>
+      <div>Reflections from the day...</div>
+    </Card>
+  );
+};
+
+export default Sidebar;
