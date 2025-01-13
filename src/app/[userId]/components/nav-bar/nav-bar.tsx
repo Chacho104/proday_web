de252baaeeae5a39b852e9da353cc1a3c28b2f28
Churchil Owino <chachowino75@gmail.com>
2025-01-13 18:48:00 +0300

    Change location of component

---
+++ src/app/[userId]/components/nav-bar/nav-bar.tsx
@@ -0,0 +1,15 @@
+// Navigation component that renders logo and user menu on every page
+
+import Logo from "./components/logo";
+import UserMenu from "./components/user-menu/user-menu";
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between w-full py-4">
+      <Logo />
+      <UserMenu />
+    </nav>
+  );
+};
+
+export default Navbar;
