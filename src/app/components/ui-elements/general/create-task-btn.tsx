612618db2dc81ef91160c349e775be1b1478ec08
Churchil Owino <chachowino75@gmail.com>
2025-01-14 13:21:33 +0300

    Add onClick prop to button

--- src/app/components/ui-elements/general/create-task-btn.tsx
+++ src/app/components/ui-elements/general/create-task-btn.tsx
@@ -7,9 +7,14 @@ import { FaPlus } from "react-icons/fa6";
 interface CreateTaskButtonProps {
   className?: string;
   label: string;
+  onClick: () => void;
 }
 
-const CreateTaskButton = ({ className, label }: CreateTaskButtonProps) => {
+const CreateTaskButton = ({
+  className,
+  label,
+  onClick,
+}: CreateTaskButtonProps) => {
   return (
     <button
       type="button"
@@ -17,6 +22,7 @@ const CreateTaskButton = ({ className, label }: CreateTaskButtonProps) => {
         "flex items-center justify-center bg-warm-yellow rounded-md gap-x-1 py-2 px-4 hover:scale-105 hover:opacity-80 transition",
         className
       )}
+      onClick={onClick}
     >
       <FaPlus size={14} />
       <span className="text-sm">{label}</span>
