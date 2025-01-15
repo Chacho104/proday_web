4abd95be19e411f88b7bd678ec186ea3e875da5d
Churchil Owino <chachowino75@gmail.com>
2025-01-15 17:41:28 +0300

    Define schema and type for task form

--- src/app/lib/schema-definitions.ts
+++ src/app/lib/schema-definitions.ts
@@ -18,13 +18,43 @@ export const AuthFormSchema = z.object({
 });
 
 // Type for the form state: useful for form submission action
-export type FormState =
+export type AuthFormState =
   | {
       errors?: {
         email?: string[];
         password?: string[];
       };
       authErrors?: string;
+      message?: string;
+    }
+  | undefined;
+
+// Schema for validating user inputs when creating a task
+export const TaskFormSchema = z.object({
+  title: z.string({ message: "Please enter a valid task title." }).trim(),
+  type: z.string({ message: "Please enter a valid task type value." }).trim(),
+  urgency: z
+    .string({ message: "Please enter a valid task urgency value." })
+    .trim(),
+  importance: z
+    .string({ message: "Please enter a valid task importance value." })
+    .trim(),
+  dueDate: z
+    .string({ message: "Please enter a valid task deadline value" })
+    .trim(),
+});
+
+// Type for the form state: useful for form submission action
+export type TaskFormState =
+  | {
+      errors?: {
+        title?: string[];
+        type?: string[];
+        urgency?: string[];
+        importance?: string[];
+        dueDate?: string[];
+      };
+      serverErrors?: string;
       message?: string;
     }
   | undefined;
