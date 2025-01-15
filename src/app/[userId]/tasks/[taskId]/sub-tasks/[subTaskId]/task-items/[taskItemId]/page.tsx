8b1318d030eccb0f59419534f2381901eb50a391
Churchil Owino <chachowino75@gmail.com>
2025-01-15 17:33:35 +0300

    Create the single task item page

---
+++ src/app/[userId]/tasks/[taskId]/sub-tasks/[subTaskId]/task-items/[taskItemId]/page.tsx
@@ -0,0 +1,22 @@
+// Single item page for an item under a sub-task
+
+import { getTaskItemDetails } from "@/app/lib/dal";
+import SubTaskItemForm from "./components/sub-task-checklist-form";
+
+const SingleSubTaskItemPage = async ({
+  params,
+}: {
+  params: Promise<{ taskId: string; subTaskId: string; taskItemId: string }>;
+}) => {
+  const { taskId, subTaskId, taskItemId } = await params;
+
+  let initialData;
+
+  // Only send a request for initial data if the taskItemId is an actual id
+  if (taskItemId !== "new") {
+    initialData = await getTaskItemDetails(subTaskId, taskItemId);
+  }
+  return <SubTaskItemForm initialData={initialData} parentId={subTaskId} />;
+};
+
+export default SingleSubTaskItemPage;
