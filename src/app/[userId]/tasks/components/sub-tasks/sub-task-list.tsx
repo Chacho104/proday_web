19fa7cb05a2b6885bfa4ee878d4f7c624f0c3719
Churchil Owino <chachowino75@gmail.com>
2025-01-15 17:39:01 +0300

    Pass in task id

--- src/app/[userId]/tasks/components/sub-tasks/sub-task-list.tsx
+++ src/app/[userId]/tasks/components/sub-tasks/sub-task-list.tsx
@@ -7,19 +7,25 @@ import SubTaskItem from "./sub-task-item";
 import TaskItemList from "../task-items/task-item-list";
 
 interface SubTaskListProps {
+  taskId: string;
   subTasks: SubTask[];
   userId: string;
   taskItems: TaskItemType[];
 }
 
-const SubTaskList = ({ subTasks, taskItems, userId }: SubTaskListProps) => {
+const SubTaskList = ({
+  taskId,
+  subTasks,
+  taskItems,
+  userId,
+}: SubTaskListProps) => {
   return (
     <ul className="mt-3 ml-4 space-y-3">
       {subTasks.map((subTask) => (
         <SubTaskItem key={subTask.id} subTask={subTask} userId={userId} />
       ))}
       {taskItems.length > 0 && (
-        <TaskItemList taskItems={taskItems} userId={userId} />
+        <TaskItemList taskItems={taskItems} userId={userId} taskId={taskId} />
       )}
     </ul>
   );
