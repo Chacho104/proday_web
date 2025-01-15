3d412f4162f3b40ae2d7dacd824b6f42c610ede5
Churchil Owino <chachowino75@gmail.com>
2025-01-15 17:37:56 +0300

    Create form for creating and updating single task item

---
+++ src/app/[userId]/tasks/[taskId]/task-items/[taskItemId]/components/task-item-form.tsx
@@ -0,0 +1,50 @@
+import Card from "@/app/components/ui-elements/general/card";
+import { TaskItem } from "@/app/lib/type-definitions";
+
+interface TaskItemFormProps {
+  initialData: TaskItem | null;
+  parentId: string;
+}
+
+const TaskItemForm = ({
+  initialData,
+  parentId,
+}: TaskItemFormProps) => {
+  return (
+    <Card>
+      {initialData ? (
+        <h2 className="text-white text-lg py-2">
+          Update the selected checklist item below
+        </h2>
+      ) : (
+        <h2 className="text-white text-lg py-2">
+          Create a checklist item below
+        </h2>
+      )}
+      <form action="" className="w-full space-y-4 md:space-y-6">
+        <div className="w-full flex flex-col gap-y-2">
+          <label htmlFor="title" className="text-warm-yellow">
+            Title
+          </label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            placeholder="Checklist Item Title"
+            defaultValue={initialData ? initialData.title : undefined}
+            required
+            className="p-2 rounded-md focus:outline-none bg-text-field-bg"
+          />
+        </div>
+        <button
+          type="submit"
+          className="border-1 bg-warm-yellow py-2 px-4 rounded-md"
+        >
+          {initialData ? "Update" : "Create"}
+        </button>
+      </form>
+    </Card>
+  );
+};
+
+export default TaskItemForm;
