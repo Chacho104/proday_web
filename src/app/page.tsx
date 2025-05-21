8f17f41f4694f0ff9433e6d4d3f24b8ec36e0307
Churchil Owino <chachowino75@gmail.com>
2025-05-21 16:34:30 +0300

    Use the verifyUserSession function to redirect user accordingly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-import { getUser } from "./lib/dal";
+import { redirect } from "next/navigation";
+import { verifyUserSession } from "./lib/session";
 
 const LandingPage = async () => {
-  const user = await getUser();
+  // On landing on this page, await verification of user session
+  // In no user session exists, redirect to login page
+  // The redirect logic is implemented in the verifyUserSession function
+
+  await verifyUserSession();
+
+  // If a valid user session is found, redirect to tasks page
+  redirect("/tasks");
 };
 
 export default LandingPage;
