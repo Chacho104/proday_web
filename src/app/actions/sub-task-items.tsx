823619189493e14ad50bbb63320bdfb956b29d04
Churchil Owino <chachowino75@gmail.com>
2025-05-22 16:08:29 +0300

    Add action for getting sub-task item details

--- src/app/actions/sub-task-items.tsx
+++ src/app/actions/sub-task-items.tsx
@@ -1,8 +1,10 @@
-// File for defining the logic for creating, updating, and deleting sub-task items
+// File for defining the CRUD logic for sub-task items
 // These are server actions: ensures that the actions are not only executed server side,
 // but also that the functions are callable from client components like forms
 
 "use server";
+
+import { cache } from "react";
 
 import { redirect } from "next/navigation";
 
@@ -142,6 +144,37 @@ export async function toggleSubTaskItemCompletion(
     console.error("Error:", error);
   }
 }
+
+// Get sub-task details for a specific sub-task
+export const getTaskItemDetails = cache(
+  async (parentId: string, checklistId: string) => {
+    const authToken = await verifyUserSession();
+
+    const url = `${process.env.NEXT_PUBLIC_PRODAY_API_URL}/tasks/${parentId}/items/${checklistId}`;
+
+    try {
+      // Send GET request to get tasks
+      const response = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${authToken}`,
+        },
+      });
+
+      if (!response.ok) {
+        const { message } = await response.json();
+        return message;
+      }
+
+      const checklistItem = await response.json();
+      return checklistItem.data;
+    } catch (error: any) {
+      console.error("Error:", error);
+      return null;
+    }
+  }
+);
 
 // Server Action for deleting a task item
 export async function deleteSubTaskItem(subTaskId: string, itemId: string) {
