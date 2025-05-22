c01953495ff58508636d2adbf05dbd6794783be9
Churchil Owino <chachowino75@gmail.com>
2025-05-22 16:11:01 +0300

    Use correct component for date

--- src/app/tasks/components/nav-bar/nav-bar.tsx
+++ src/app/tasks/components/nav-bar/nav-bar.tsx
@@ -1,12 +1,12 @@
 // Navigation component that renders logo and user menu on every page
 
-import Logo from "./components/today";
+import Today from "./components/today";
 import UserMenu from "./components/user-menu/user-menu";
 
 const Navbar = () => {
   return (
     <nav className="flex items-center justify-between w-full py-4">
-      <Logo />
+      <Today />
       <UserMenu />
     </nav>
   );
